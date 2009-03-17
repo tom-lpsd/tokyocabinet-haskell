7a6f8fc4cae477d544d5e3ac181b8434d7cd28b8
tom.lpsd <tom.lpsd@gmail.com>
2009-03-17 22:19:53 +0900

    added tclist test (dump, load, marshal code, etc...)
    Ignore-this: 47fe130b2e6dfd3f9354802d9712bcbe
    
    darcs-hash:20090317131953-69edc-32952b9bce6b59575385f2facf909a4ec5f17309.gz

--- tests/ListTest.hs
+++ tests/ListTest.hs
@@ -2,6 +2,11 @@ module Main where
 import Test.HUnit
 import Control.Monad
 
+import Data.Int
+import Data.Word
+import qualified Data.ByteString.Char8 as S
+import qualified Data.ByteString.Lazy.Char8 as L
+
 import Database.TokyoCabinet.List
 
 len_test = do
@@ -111,15 +116,97 @@ bsearch_test = do
         , (@?= 2) `fmap` bsearch xs y ] join  
   delete xs
 
+int_test = do
+  xs <- new
+  let num_int   = [ 1..10] :: [Int]
+      num_int8  = [11..20] :: [Int8]
+      num_int16 = [21..30] :: [Int16]
+      num_int32 = [31..40] :: [Int32]
+      num_int64 = [41..50] :: [Int64]
+  pushlist xs num_int
+  pushlist xs num_int8
+  pushlist xs num_int16
+  pushlist xs num_int32
+  pushlist xs num_int64
+  join $ (@?= (Just (num_int   !! 0))) `fmap` get xs 0
+  join $ (@?= (Just (num_int   !! 9))) `fmap` get xs 9
+  join $ (@?= (Just (num_int8  !! 0))) `fmap` get xs 10
+  join $ (@?= (Just (num_int8  !! 9))) `fmap` get xs 19
+  join $ (@?= (Just (num_int16 !! 0))) `fmap` get xs 20
+  join $ (@?= (Just (num_int16 !! 9))) `fmap` get xs 29
+  join $ (@?= (Just (num_int32 !! 0))) `fmap` get xs 30
+  join $ (@?= (Just (num_int32 !! 9))) `fmap` get xs 39
+  join $ (@?= (Just (num_int64 !! 0))) `fmap` get xs 40
+  join $ (@?= (Just (num_int64 !! 9))) `fmap` get xs 49
+    where
+      pushlist xs ys = mapM_ (push xs) ys
+
+word_test = do
+  xs <- new
+  let num_word8   = [ 1..10] :: [Word8]
+      num_word16  = [11..20] :: [Word16]
+      num_word32  = [21..30] :: [Word32]
+      num_word64  = [31..40] :: [Word64]
+  pushlist xs num_word8
+  pushlist xs num_word16
+  pushlist xs num_word32
+  pushlist xs num_word64
+  join $ (@?= (Just (num_word8  !! 0))) `fmap` get xs 0
+  join $ (@?= (Just (num_word8  !! 9))) `fmap` get xs 9
+  join $ (@?= (Just (num_word16 !! 0))) `fmap` get xs 10
+  join $ (@?= (Just (num_word16 !! 9))) `fmap` get xs 19
+  join $ (@?= (Just (num_word32 !! 0))) `fmap` get xs 20
+  join $ (@?= (Just (num_word32 !! 9))) `fmap` get xs 29
+    where
+      pushlist xs ys = mapM_ (push xs) ys
+
+strict_bytestring_test = do
+  xs <- new
+  let vals = map S.pack ["foo", "bar", "baz"]
+  mapM_ (push xs) vals
+  join $ (@?= (Just (vals  !! 0))) `fmap` get xs 0
+  join $ (@?= (Just (vals  !! 1))) `fmap` get xs 1
+  join $ (@?= (Just (vals  !! 2))) `fmap` get xs 2  
+
+lazy_bytestring_test = do
+  xs <- new
+  let vals = map L.pack ["foo", "bar", "baz"]
+  mapM_ (push xs) vals
+  join $ (@?= (Just (vals  !! 0))) `fmap` get xs 0
+  join $ (@?= (Just (vals  !! 1))) `fmap` get xs 1
+  join $ (@?= (Just (vals  !! 2))) `fmap` get xs 2  
+
+dump_load_test = do
+  xs <- new
+  mapM_ (push xs) ["foo", "bar", "baz"]
+  bytes <- dump xs
+  ys <- load bytes
+  xsiz <- len xs
+  ysiz <- len ys
+  xsiz @?= ysiz
+  tclist_equal xs ys 0 xsiz
+  where
+    tclist_equal xs ys ix num =
+        do if ix == num
+             then return ()
+             else do liftM2 (@?=) (get xs ix :: IO (Maybe String))
+                                  (get ys ix :: IO (Maybe String))
+                     tclist_equal xs ys (1 + ix) num
+
 tests = test [
-          len_test
-        , push_pop_test
-        , unshift_shift_test
-        , clear_test
-        , insert_remove_test
-        , sort_test
-        , lsearch_test
-        , bsearch_test
+          "len test" ~: len_test
+        , "push pop test" ~: push_pop_test
+        , "unshift shift test" ~: unshift_shift_test
+        , "clear test" ~: clear_test
+        , "insert remove test" ~: insert_remove_test
+        , "sort test" ~: sort_test
+        , "lsearch test" ~: lsearch_test
+        , "bsearch test" ~: bsearch_test
+        , "int_test" ~: int_test
+        , "word_test" ~: word_test
+        , "strict_bytestring_test" ~: strict_bytestring_test
+        , "lazy_bytestring_test" ~: lazy_bytestring_test
+        , "dump_load_test" ~: dump_load_test
         ]
 
 main = runTestTT tests
