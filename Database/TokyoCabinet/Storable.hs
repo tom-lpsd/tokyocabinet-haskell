071e7c8f1a8f89f68775faf01740f8aff8dc01c1
tom.lpsd <tom.lpsd@gmail.com>
2009-03-20 01:42:57 +0900

    Extend Storable class (now we can store [Int], [Double] etc..)
    Ignore-this: f6146912571fe2c40db47495635fdb49
    
    darcs-hash:20090319164257-69edc-52f95dcf4274292039147eb811e3bfa89f97bf05.gz

--- Database/TokyoCabinet/Storable.hs
+++ Database/TokyoCabinet/Storable.hs
@@ -8,6 +8,7 @@ import Foreign.Ptr
 import Foreign.C.Types
 import Foreign.C.String
 import Foreign.Marshal (peekArray, free)
+import Foreign.Marshal.Array (withArray)
 import Data.ByteString.Unsafe
 
 import qualified Foreign.Storable as F
@@ -25,6 +26,10 @@ liftPL f val action =
 class Storable a where
     withPtrLen :: a -> (PtrLen -> IO b) -> IO b
     peekPtrLen :: PtrLen -> IO a
+    withPtrLenL :: [a] -> (PtrLen -> IO b) -> IO b
+    peekPtrLenL :: PtrLen -> a -> IO [a]
+    withPtrLenL = undefined
+    peekPtrLenL = undefined
 
 instance Storable S.ByteString where
     withPtrLen = liftPL unsafeUseAsCStringLen
@@ -37,13 +42,6 @@ instance Storable L.ByteString where
                              free p
                              return $ L.pack xs
 
-instance Storable String where
-    withPtrLen = liftPL withCStringLen
-    peekPtrLen (buf, siz) = do
-      val <- peekCStringLen (castPtr buf, fromIntegral siz)
-      free buf
-      return val
-
 withPtrLenForFStorable :: (F.Storable a) => a -> (PtrLen -> IO b) -> IO b
 withPtrLenForFStorable n f =
     U.with n $ \p -> f (castPtr p, fromIntegral $ F.sizeOf n)
@@ -53,55 +51,108 @@ peekPtrLenForFStorable (p, _) = do val <- F.peek (castPtr p)
                                    free p
                                    return val
 
+withPtrLenLForFStorable :: (F.Storable a) => [a] -> (PtrLen -> IO b) -> IO b
+withPtrLenLForFStorable xs f =
+    withArray xs $ \p ->
+        f (castPtr p, fromIntegral $ (F.sizeOf $ head xs) * length xs)
+
+peekPtrLenLForFStorable :: (F.Storable a) => PtrLen -> a -> IO [a]
+peekPtrLenLForFStorable (p, size) x = do
+  peekArray (fromIntegral size `div` (F.sizeOf x)) (castPtr p)
+
+instance Storable Char where
+    withPtrLen = withPtrLenForFStorable
+    peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = liftPL withCStringLen
+    peekPtrLenL (buf, siz) _ =
+        do val <- peekCStringLen (castPtr buf, fromIntegral siz)
+           free buf
+           return val
+
 instance Storable CInt where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Int where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable CDouble where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Double where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable CFloat where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
+
+instance Storable Float where
+    withPtrLen = withPtrLenForFStorable
+    peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Int8 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Int16 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Int32 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Int64 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Word8 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Word16 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Word32 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
 instance Storable Word64 where
     withPtrLen = withPtrLenForFStorable
     peekPtrLen = peekPtrLenForFStorable
+    withPtrLenL = withPtrLenLForFStorable
+    peekPtrLenL = peekPtrLenLForFStorable
 
+instance (F.Storable a, Storable a) => Storable [a] where
+    withPtrLen = withPtrLenL
+    peekPtrLen xs = peekPtrLenL xs undefined
